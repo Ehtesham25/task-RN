1cb1fe337dc129a379d4b676312544ae8d284680
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-03-01 10:10:36 +0500

    Icons--

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import landing from './landing.png';
+import oval from './oval.png';
+import fb from './fb.png';
+import arrowRight from './arrowRight.png';
+import leftOval from './leftOval.png';
+import login from './login.png';
+import username from './username.png';
+import eye from './eye.png';
+import lock from './lock.png';
+import car from './car.png';
+import car1 from './car1.png';
+import car2 from './car2.png';
+import car3 from './car3.png';
+import map from './map.png';
+import name from './name.png';
+
+export const images = {
+  landing,
+  oval,
+  fb,
+  arrowRight,
+  leftOval,
+  login,
+  username,
+  eye,
+  lock,
+  car,
+  car1,
+  car2,
+  car3,
+  map,
+  name,
+};
