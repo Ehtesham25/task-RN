29649bff66623180b6a9002f4075988216cb31c5
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-03-01 18:43:04 +0500

    items

--- App.tsx
+++ App.tsx
@@ -1,40 +1,22 @@
-import {
-  Landing,
-  Login,
-  Register,
-  AddNew,
-  Cars,
-  Profile,
-  Location,
-} from './src/screens';
-import {WelcomeLanding} from './src/components';
+import React from 'react';
 import {SafeAreaView} from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
+import Items from './src/screens/Items';
 
 export default function App() {
+  const [items, setItems] = React.useState();
+
+  React.useEffect(() => {
+    fetch('https://dummyjson.com/carts')
+      .then(data => data.json())
+      .then(res => {
+        setItems(res.carts);
+      });
+  }, []);
+
   return (
     <>
-      <SafeAreaView style={{backgroundColor: '#C6AB59'}} />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-          initialRouteName="wellcome">
-          <Stack.Screen name="wellcome" component={WelcomeLanding} />
-          <Stack.Screen name="landing" component={Landing} />
-          <Stack.Screen name="login" component={Login} />
-          <Stack.Screen name="register" component={Register} />
-          <Stack.Screen name="cars" component={Cars} />
-          <Stack.Screen name="addnew" component={AddNew} />
-          <Stack.Screen name="profile" component={Profile} />
-          <Stack.Screen name="location" component={Location} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Items data={items} />
     </>
   );
 }
