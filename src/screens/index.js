d66d752d5c0606b9d468da7f65720b73d7d7a619
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-03-01 10:14:27 +0500

    add new

---
+++ src/screens/index.js
@@ -0,0 +1,7 @@
+export {default as Landing} from './Landing';
+export {default as Login} from './Login';
+export {default as Register} from './Register';
+export {default as Cars} from './Cars';
+export {default as AddNew} from './AddNew';
+export {default as Profile} from './Profile';
+export {default as Location} from './Location';
