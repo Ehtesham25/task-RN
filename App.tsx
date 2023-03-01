5a6526f40b2c2ff6d42f7fdb0132bf902b4d591d
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-03-01 10:15:15 +0500

    app tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import {
+  Landing,
+  Login,
+  Register,
+  AddNew,
+  Cars,
+  Profile,
+  Location,
+} from './src/screens';
+import {WelcomeLanding} from './src/components';
+import {SafeAreaView} from 'react-native';
 
-import Tab from './src/screens/components/Tab';
-import Form from './src/screens/Form';
-import {ContextProvider} from './src/context';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [state, setState] = React.useState('Hooks');
-  const [formValue, setFormValue] = React.useState();
   return (
     <>
-      <ContextProvider>
-        <Form state={state} formValue={formValue} setFormValue={setFormValue} />
-        <Tab setState={setState} state={state} />
-      </ContextProvider>
+      <SafeAreaView style={{backgroundColor: '#C6AB59'}} />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="wellcome">
+          <Stack.Screen name="wellcome" component={WelcomeLanding} />
+          <Stack.Screen name="landing" component={Landing} />
+          <Stack.Screen name="login" component={Login} />
+          <Stack.Screen name="register" component={Register} />
+          <Stack.Screen name="cars" component={Cars} />
+          <Stack.Screen name="addnew" component={AddNew} />
+          <Stack.Screen name="profile" component={Profile} />
+          <Stack.Screen name="location" component={Location} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
