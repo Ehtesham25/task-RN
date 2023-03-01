a38931c00e848e42885e1cb6852b8667c50d952e
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-03-01 10:14:09 +0500

    cars add

---
+++ src/data/index.js
@@ -0,0 +1,103 @@
+import {images} from "../assets"
+
+export const product = {
+  title: 'SOFT MINI CROSSBODY BAG WITH KISS LOCK',
+  description: [
+    'Mini crossbody bag available in various colours. Featuring two compartments. Handles and detachable crossbody shoulder strap. Lined interior. Clasp with two metal pieces.',
+    'Height x Length x Width: 14 x 21.5 x 4.5 cm. / 5.5 x 8.4 x 1.7"',
+  ],
+  price: '29.99£',
+};
+
+export const menuData = {
+  title: 'Product',
+  subCatogries: ['item1', 'item2', 'item3', 'item4'],
+};
+
+export const quotes = [
+  {
+    quote:
+      'For the things we have to learn before we can do them, we learn by doing them.',
+  },
+  {
+    quote: 'The fastest way to build an app.',
+  },
+  {
+    quote:
+      'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing.',
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+  },
+  {
+    quote:
+      'If life were predictable it would cease to be life, and be without flavor.',
+  },
+  {
+    quote:
+      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+  },
+  {
+    quote:
+      "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+  },
+];
+
+
+export const cars= [
+  {
+    name:"Toyota",
+    model:"2022",
+    image:images.car,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Suzuki",
+    model:"2021",
+    image:images.car1,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Hundwai",
+    model:"2020",
+    image:images.car2,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Honda Civic",
+    model:"2019",
+    image:images.car3,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Toyota",
+    model:"2022",
+    image:images.car,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Suzuki",
+    model:"2021",
+    image:images.car1,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Hundwai",
+    model:"2020",
+    image:images.car2,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+  {
+    name:"Honda Civic",
+    model:"2019",
+    image:images.car3,
+    desc:'Lorem ipsum is a placeholder commonly used '
+  },
+]
---
+++ src/screens/Cars.js
@@ -0,0 +1,112 @@
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+import {RFValue} from 'react-native-responsive-fontsize';
+
+import {cars} from '../data';
+import {Box, Button, MyText, Icon} from '../components';
+import {images} from '../assets';
+
+const Car = ({item}) => {
+  const {name, model, desc, image} = item;
+  return (
+    <Box
+      style={styles.container}
+      full
+      df
+      row
+      pad={`${wp(5)}px ${wp(21)}px  ${wp(5)}px 0`}
+      mar={`${wp(2)}px`}>
+      <Box centerAll hasRadius={100}>
+        <Image source={image} style={styles.image} />
+      </Box>
+      <Box>
+        <MyText bold fs={RFValue(14)}>
+          {name}({model})
+        </MyText>
+        <MyText pad={`0 ${wp(5)}px 0 0`} fs={RFValue(13)}>
+          {desc}
+        </MyText>
+      </Box>
+    </Box>
+  );
+};
+
+const Cars = ({navigation}) => {
+  return (
+    <Box full relative>
+      <Box zIndx="1000" between row mar={`${wp(4)}px} 0 0 0`}>
+        <TouchableOpacity onPress={() => navigation.navigate('location')}>
+          <Image source={images.map} style={styles.touchaOpacity} />
+        </TouchableOpacity>
+        <MyText fs={RFValue(20)} bold>
+          Cars List
+        </MyText>
+        <TouchableOpacity onPress={() => navigation.navigate('profile')}>
+          <Image source={images.username} style={styles.touchaOpacity} />
+        </TouchableOpacity>
+      </Box>
+      <Box mar={`0 0 ${wp(50)}px 0`}>
+        <FlatList
+          data={cars}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({item}) => {
+            return (
+              <>
+                <Box full>
+                  <Car item={item} />
+                </Box>
+              </>
+            );
+          }}
+        />
+      </Box>
+      <Button
+        absolute
+        bottom="30"
+        right="20"
+        text="+"
+        fs={RFValue(25)}
+        hasRadius={100}
+        bg="#32baa5"
+        wd={wp(14)}
+        ht={wp(14)}
+        color="#fff"
+        onPress={() => navigation.navigate('addnew')}
+      />
+    </Box>
+  );
+};
+
+export default Cars;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#32baa577',
+    borderRadius: 5,
+    borderColor: 'white',
+    shadowRadius: 3,
+    shadowOpacity: 1,
+    shadowColor: 'grey',
+    shadowOffset: {
+      width: 3,
+      height: 5,
+    },
+  },
+  image: {
+    width: 100,
+    height: 60,
+    resizeMode: 'contain',
+  },
+  touchaOpacity: {
+    width: wp(8),
+    height: wp(8),
+    resizeMode: 'contain',
+  },
+});
