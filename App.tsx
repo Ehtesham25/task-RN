e616692f1b62b92f3d4473676c56edacbda5df92
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2023-03-02 12:32:19 +0500

    filter data

--- App.tsx
+++ App.tsx
@@ -7,23 +7,44 @@ import Items from './src/screens/Items';
 export default function App() {
   const [items, setItems] = React.useState();
   const [cartItems, seCartItems] = React.useState([]);
+  const [search, setSearch] = React.useState('');
 
   const getData = async () => {
     const data: any = await axios.get('https://dummyjson.com/carts');
     setItems(data);
   };
 
-  React.useEffect(() => {
+  const handleFetchData = () => {
     fetch('https://dummyjson.com/carts')
       .then(data => data.json())
       .then(res => {
         setItems(res.carts);
       });
-  }, []);
+  };
+  React.useEffect(() => {}, []);
 
+  React.useEffect(() => {
+    if (search) {
+      setItems(
+        items.filter((item: any) =>
+          item.products[0].title
+            .toLowerCase()
+            .includes(search.toLocaleLowerCase()),
+        ),
+      );
+    } else {
+      handleFetchData();
+    }
+  }, [search]);
   return (
     <>
-      <Items data={items} cartItems={cartItems} setCartItems={seCartItems} />
+      <Items
+        data={items}
+        cartItems={cartItems}
+        setCartItems={seCartItems}
+        search={search}
+        setSearch={setSearch}
+      />
     </>
   );
 }
